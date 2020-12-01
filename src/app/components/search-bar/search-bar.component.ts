67877bdc699b89342fd0a947e6b3d652f09f16ff
benjuniour <benjuniour@users.noreply.github.com>
2020-11-30 23:37:24 -0500

    searching by course title works

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -9,12 +9,30 @@ export class SearchBarComponent implements OnInit {
   
   dataset: string[];
   searchText :string;
+  showSearchResults: boolean = true;
+  searchResult: string;
 
   constructor() { 
-    this.dataset = ['hello', 'boy', 'boss', 'toy', 'samplenote']
+    this.dataset = [
+      'HIST 151', 
+      'BIO 223', 
+      'CS 336', 
+      'CS 108', 
+      'HIST 153',
+      'PHIL 153',
+      'ENG 101',
+      'ENG 102',
+      'ENGR 220',
+      'PER 103'
+    ]
   }
 
   ngOnInit(): void {
   }
 
+  displaySearchResults(desiredSearchVal: string): void {
+    this.searchResult = desiredSearchVal
+    this.searchText = ""
+   
+  }
 }
